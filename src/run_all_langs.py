17fa96c447c0aad557a7b7c7c819a3b38cfe4e2e
roeeaharoni <roee.aharoni@gmail.com>
2016-04-18 16:33:48 +0300

    changed run_all_langs to use nfst
    changed run_all_langs to use nfst

--- src/run_all_langs.py
+++ src/run_all_langs.py
@@ -74,13 +74,21 @@ def train_language(cnn_mem, epochs, feat_input_dim, hidden_dim, input_dim, lang,
                 sigmorphon_root_dir, src_dir):
     start = time.time()
     os.chdir(src_dir)
-    os.system('python task1_joint_structured_inflection_blstm_feedback_fix.py --cnn-mem {0} --input={1} --hidden={2} \
+    os.system('python task1_joint_structured_inflection_neural_fst.py --cnn-mem {0} --input={1} --hidden={2} \
         --feat-input={3} --epochs={4} --layers={5} --optimization {6} \
         {7}/data/{8}-task1-train \
         {7}/data/{8}-task1-dev \
-        {9}/joint_strc_blstm_feed_fix_{8}_results.txt \
+        {9}/nfst_{8}-results.txt \
         {7}'.format(cnn_mem, input_dim, hidden_dim, feat_input_dim, epochs, layers, optimization,
                     sigmorphon_root_dir, lang, results_dir))
+
+    # os.system('python task1_joint_structured_inflection_blstm_feedback_fix.py --cnn-mem {0} --input={1} --hidden={2} \
+    #     --feat-input={3} --epochs={4} --layers={5} --optimization {6} \
+    #     {7}/data/{8}-task1-train \
+    #     {7}/data/{8}-task1-dev \
+    #     {9}/joint_strc_blstm_feed_fix_{8}_results.txt \
+    #     {7}'.format(cnn_mem, input_dim, hidden_dim, feat_input_dim, epochs, layers, optimization,
+    #                 sigmorphon_root_dir, lang, results_dir))
 
     # os.system('python task1_factored_inflection.py --cnn-mem 4096 --input=150 --hidden=150 --epochs=300
     # --layers=2 \
