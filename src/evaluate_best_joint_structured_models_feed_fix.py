103a879c1c1d7e58c4f4ee294612e4b8fb01a531
roeeaharoni <roee.aharoni@gmail.com>
2016-04-19 15:50:24 +0300

    changed eval script for joint fixed to print less
    changed eval script for joint fixed to print less

--- src/evaluate_best_joint_structured_models_feed_fix.py
+++ src/evaluate_best_joint_structured_models_feed_fix.py
@@ -140,7 +140,7 @@ def main(train_path, test_path, results_file_path, sigmorphon_root_dir, input_di
 
             accuracy = task1_joint_structured_inflection_feedback_fix.evaluate_model(predicted_templates, test_cluster_lemmas,
                                                                         test_cluster_feat_dicts, test_cluster_words,
-                                                                        feature_types, True)
+                                                                        feature_types, print_results=False)
             accuracies.append(accuracy)
 
             # get predicted_templates in the same order they appeared in the original file
