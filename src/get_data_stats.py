b0ab288e8d899e6e27d76f9fdad24a9021d23c1c
roeeaharoni <roee.aharoni@gmail.com>
2016-04-21 22:42:31 +0300

    added more stats to get_data_stats
    added more stats to get_data_stats

--- src/get_data_stats.py
+++ src/get_data_stats.py
@@ -3,7 +3,7 @@ import common
 
 def main():
 
-    langs = ['turkish', 'navajo', 'german', 'arabic', 'finnish', 'georgian', 'spanish', 'russian', 'hungarian', 'maltese']
+    langs = ['russian', 'georgian', 'finnish', 'arabic', 'navajo', 'spanish', 'turkish', 'german', 'hungarian', 'maltese']
     for lang in langs:
         train_path = '/Users/roeeaharoni/GitHub/sigmorphon2016/data/{0}-task1-train'.format(lang)
         dev_path = '/Users/roeeaharoni/GitHub/sigmorphon2016/data/{0}-task1-dev'.format(lang)
@@ -18,6 +18,7 @@ def main():
         for cluster in train_cluster_to_data_indices:
             train_agg += len(train_cluster_to_data_indices[cluster])
             print 'train ' + lang + ' ' + cluster + ' : ' + str(len(train_cluster_to_data_indices[cluster])) + ' examples'
+
         print 'train ' + lang + ' ' + 'agg' + ' : ' + str(train_agg) + ' examples'
         dev_agg = 0
         for cluster in test_cluster_to_data_indices:
@@ -28,5 +29,72 @@ def main():
         print lang + ' avg ex. per morph: ' + str(sum([len(l) for l in train_morph_to_data_indices.keys()])/float(len(train_morph_to_data_indices)))
         print lang + ' dev morphs: ' + str(len(test_morph_to_data_indices))
         print lang + ' num features: ' + str(len(feature_types))
+
+        for cluster in train_cluster_to_data_indices:
+            print 'train ' + lang + ' ' + cluster + ' : ' + str(
+                len(train_cluster_to_data_indices[cluster])) + ' examples'
+            train_cluster_words = [train_words[i] for i in train_cluster_to_data_indices[cluster]]
+            train_cluster_lemmas = [train_lemmas[i] for i in train_cluster_to_data_indices[cluster]]
+            prefix_count, suffix_count, same_count, circumfix_count, other_count, lev_avg = get_morpheme_stats(
+                train_cluster_words,
+                train_cluster_lemmas)
+            print "train {0} {1} {2} &  {3} & {4} & {5} & {6} & {7:.3f}".format(
+                lang, cluster, prefix_count, suffix_count, same_count, circumfix_count, other_count, lev_avg)
+
+        prefix_count, suffix_count, same_count, circumfix_count, other_count, lev_avg = get_morpheme_stats(
+            train_words,
+            train_lemmas)
+        print "train {0} {1} {2} &  {3} & {4} & {5} & {6} & {7:.3f}".format(
+            lang, 'AGG', prefix_count, suffix_count, same_count, circumfix_count, other_count, lev_avg)
+
+def get_morpheme_stats(train_lemmas, train_words):
+    prefix_count = 0
+    suffix_count = 0
+    other_count = 0
+    circumfix_count = 0
+    same_count = 0
+    lev_sum = 0
+    for lemma, word in zip(train_lemmas, train_words):
+        if lemma == word:
+            same_count+=1
+            continue
+
+        if lemma in word:
+            if word.replace(lemma, '') + lemma == word:
+                prefix_count += 1
+            if lemma + word.replace(lemma, '') == word:
+                suffix_count += 1
+            if lemma + word.replace(lemma, '') != word and word.replace(lemma, '') + lemma != word:
+                circumfix_count +=1
+        else:
+            other_count +=1
+
+        lev_sum += levenshtein(lemma, word)
+
+
+    return prefix_count, suffix_count, same_count, circumfix_count, other_count, lev_sum/float(len(train_lemmas))
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
 if __name__ == '__main__':
     main()
+
