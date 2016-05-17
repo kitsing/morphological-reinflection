32a848a11bf2b7a67009bfa0316576d369997591
roeeaharoni <roee.aharoni@gmail.com>
2016-05-17 13:52:04 +0300

    added print to factored model to show when skipping an existing model
    added print to factored model to show when skipping an existing model

--- src/task1_factored_inflection.py
+++ src/task1_factored_inflection.py
@@ -280,6 +280,7 @@ def train_model(model, encoder_frnn, encoder_rrnn, decoder_rnn, train_morph_word
     # TODO: remove this later, it's a temporary fix to avoid re-training already trained models
     tmp_model_path = results_file_path + '_' + morph_index + '_bestmodel.txt'
     if os.path.isfile(tmp_model_path):
+        print '\n\n\n**********************************SKIPPING {}*******************************\n\n\n'.format(tmp_model_path)
         return model
 
     np.random.seed(17)
