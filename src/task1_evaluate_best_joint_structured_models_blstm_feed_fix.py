7497e1cfb6147e421720c992f756d923496c86fa
roeeaharoni <roee.aharoni@gmail.com>
2016-06-19 16:43:13 +0300

    added debug print
    added debug print

--- src/task1_evaluate_best_joint_structured_models_blstm_feed_fix.py
+++ src/task1_evaluate_best_joint_structured_models_blstm_feed_fix.py
@@ -123,6 +123,7 @@ def main(train_path, test_path, results_file_path, sigmorphon_root_dir, input_di
         # handle model ensemble
         if ensemble:
             ensemble_model_names = ensemble.split(',')
+            print ensemble_model_names
             ensemble_models = []
             for e in ensemble_model_names:
                 model, encoder_frnn, encoder_rrnn, decoder_rnn = load_best_model(str(cluster_index),
