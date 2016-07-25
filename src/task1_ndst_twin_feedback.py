a4b246b336b3046f6ce13a24134048a665ef801d
roeeaharoni <roee.aharoni@gmail.com>
2016-07-25 13:45:35 +0300

    removed prints
    removed prints

--- src/task1_ndst_twin_feedback.py
+++ src/task1_ndst_twin_feedback.py
@@ -733,8 +733,8 @@ def one_word_loss(model, encoder_frnn, encoder_rrnn, decoder_rnn, char_feedback_
             prev_output_vec = tanh(feedback_R * concatenate([c_f_state.output(), a_s_state.output()]) + feedback_bias)
 
             i += 1
-    print u'in:  {}\nout: {}\npredicted: {}'.format(aligned_lemma, aligned_word, predicted_seq)
-    print u'actions: {}\nchars:{}'.format(action_feedback_seq, char_feedback_seq)
+    # print u'in:  {}\nout: {}\npredicted: {}'.format(aligned_lemma, aligned_word, predicted_seq)
+    # print u'actions: {}\nchars:{}'.format(action_feedback_seq, char_feedback_seq)
     # TODO: maybe here a "special" loss function is appropriate?
     # loss = esum(loss)
     loss = average(loss)
